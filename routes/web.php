791792aeaf4f797fbc5759763fa8d803c5f73aa0
agilcore09 <agiljibrin009@gmail.com>
2023-10-07 20:46:57 +0800

    sudah ada dashboard login

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,10 @@ Route::get('/', function () {
     return view('portal');
 });
 
-Route::get('/register', function(){
-    return "test sudah bisa";
+Route::get('/login', function(){
+    return view('dashboard.login');
 });
- 
+
+Route::get('/register', function(){
+    return view('dashboard.register');
+});
